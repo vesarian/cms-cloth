9345a3bc8f75599c0dd8144bdefc2fd5e96104dd
vesarian <vesariaan@gmail.com>
2023-07-16 02:45:49 +0700

    minor edit

--- server/models/Cloth.go
+++ server/models/Cloth.go
@@ -2,13 +2,13 @@ package models
 
 type Cloth struct {
 	GormModel
-	Name        string `gorm:"column:name;not null" json:"name" form:"name" valid:"required"`
-	Description string `gorm:"column:description" json:"description" form:"description"`
-	Price       int    `gorm:"column:price;not null" json:"price" form:"price"`
-	ImgUrl      string `gorm:"column:imgUrl" json:"imgUrl" form:"imgUrl"`
-	Stock       int    `gorm:"column:stock;not null" json:"stock" form:"stock"`
-	UserId    uint   `gorm:"column:UserId" json:"UserId" form:"UserId`
-	User        *User  `json:"-"`
-	CategoryId  uint   `gorm:"column:categoryId" json:"categoryId" form:"categoryId"`
-	Category    *Category
+	Name        string    `gorm:"column:name;not null" json:"name" form:"name" valid:"required"`
+	Description string    `gorm:"column:description" json:"description" form:"description"`
+	Price       int       `gorm:"column:price;not null" json:"price" form:"price"`
+	ImgUrl      string    `gorm:"column:imgUrl" json:"imgUrl" form:"imgUrl"`
+	Stock       int       `gorm:"column:stock;not null" json:"stock" form:"stock"`
+	UserId      uint      `gorm:"column:user_id" json:"user_id" form:"user_id"`
+	User        *User     `json:"-"`
+	CategoryId  uint      `gorm:"column:category_id" json:"category_id" form:"category_id"`
+	Category    *Category `json:"-"`
 }
